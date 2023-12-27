2169a7adc6dfbc6335355c2321684fdb1d596822
Harvey Yemm <hjyemm@icloud.com>
2023-12-27 01:30:40 +0000

    Started add function to navbar

--- client/src/component/Navbar.jsx
+++ client/src/component/Navbar.jsx
@@ -1,12 +1,32 @@
 import React from 'react'
 import Style from '../style/navbar.module.css'
+import PDM from '../assets/PDMLogo.avif'
+import { Link } from 'react-router-dom'
 
 export const Navbar = () => {
   return (
     <>
         <div className={Style.container}>
           <div className={Style.navbar}>
-          
+            <div className={Style.top}>
+              <div className={Style.item}>
+                <Link to={'/'}><img src={PDM} alt="Logo" /></Link>
+              </div>
+              <div className={Style.item}>
+                <ul>
+                  <li></li>
+                  <li></li>
+                  <li></li>
+                  <li></li>
+                </ul>
+              </div>
+            </div>
+            <div className={Style.bottom}>
+              <ul>
+                <li>Fragrances</li>
+                <li>Collections</li>
+              </ul>
+            </div>
           </div>
         </div>
     </>
