e867401f8851d8f7df3d3a57c7021bb498d93965
Harvey Yemm <hjyemm@icloud.com>
2023-12-27 11:20:43 +0000

    Added: Auth Component

--- client/src/page/Account.jsx
+++ client/src/page/Account.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import { Auth } from '../utils/Auth'
 
 export const Account = () => {
   return (
     <React.Fragment>
-      
+      <Auth/>
     </React.Fragment>
   )
 }
