79e1776895e035c447b1d9c50c98c076faed14d4
Harvey Yemm <hjyemm@icloud.com>
2023-12-27 10:44:05 +0000

    Completed Navbar

--- client/src/component/Navbar.jsx
+++ client/src/component/Navbar.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Style from '../style/navbar.module.css'
 import PDM from '../assets/PDMLogo.avif'
 import { Link } from 'react-router-dom'
+import { User, MagnifyingGlass, Bag  } from 'phosphor-react'
 
 export const Navbar = () => {
   return (
@@ -10,21 +11,43 @@ export const Navbar = () => {
           <div className={Style.navbar}>
             <div className={Style.top}>
               <div className={Style.item}>
-                <Link to={'/'}><img src={PDM} alt="Logo" /></Link>
+            
               </div>
               <div className={Style.item}>
-                <ul>
-                  <li></li>
-                  <li></li>
-                  <li></li>
-                  <li></li>
-                </ul>
+              <div className={Style.logo}>
+                <Link to={'/'}><img src={PDM} alt="Logo" /></Link>
+               </div>
+              </div>
+              <div className={Style.item}>
+                <div className={Style.icons}>
+                  <ul>
+                    <li><Link to={'/account'}><User size={24}/></Link></li>
+                    <li><MagnifyingGlass size={24}/></li>
+                    <li><Link to={'/Cart'}><Bag size={24}/></Link></li>
+                  </ul>
+                </div>
               </div>
             </div>
             <div className={Style.bottom}>
               <ul>
-                <li>Fragrances</li>
-                <li>Collections</li>
+                <li>
+                  <Link>Holiday</Link>
+                </li>
+                <li>
+                  <Link>Gift Guide</Link>
+                </li>
+                <li>
+                  <Link>Fragrances</Link>
+                </li>
+                <li>
+                  <Link>Collections</Link>
+                </li>
+                <li>
+                  <Link>Body Line</Link>
+                </li>
+                <li>
+                  <Link>Maiso</Link>
+                </li>
               </ul>
             </div>
           </div>
