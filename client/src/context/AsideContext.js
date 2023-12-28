2e99cc8cedaba8e5148cc81457c434ccc55480a9
Harvey Yemm <hjyemm@icloud.com>
2023-12-28 09:05:38 +0000

    Created Aside Hook

---
+++ client/src/context/AsideContext.js
@@ -0,0 +1,28 @@
+// AsideContext.js
+import React, { createContext, useContext, useState } from 'react';
+import { Aside } from '../component/Aside';
+
+const AsideContext = createContext();
+
+export const AsideProvider = ({ children }) => {
+  const [isAsideVisible, setIsAsideVisible] = useState(true);
+
+  const showAside = () => {
+    setIsAsideVisible(true);
+  };
+
+  const hideAside = () => {
+    setIsAsideVisible(false);
+  };
+
+  return (
+    <AsideContext.Provider value={{ isAsideVisible, showAside, hideAside }}>
+      <Aside/>
+      {children}
+    </AsideContext.Provider>
+  );
+};
+
+export const useAside = () => {
+  return useContext(AsideContext);
+};
