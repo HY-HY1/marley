1cb37f012aa46631c8d1dbbaa5ef1950ed0ce653
Harvey Yemm <hjyemm@icloud.com>
2023-12-28 09:09:02 +0000

    Created Aside component

--- client/src/component/Aside.jsx
+++ client/src/component/Aside.jsx
@@ -1,46 +1,58 @@
-import React from 'react'
-import Style from '../style/aside.module.css'
-import { Button } from './Button'
+// Aside.js
+import React, { useEffect } from 'react';
+import Style from '../style/aside.module.css';
+import { Button } from './Button';
+import { useAside } from '../context/AsideContext';
 
-export const Aside = ({ visible }) => {
+export const Aside = () => {
+  const { isAsideVisible, hideAside } = useAside();
+
+  useEffect(() => {
+    
+  })
+
   return (
     <React.Fragment>
-        {visible ? (
-                    <div className={Style.container}>
-                    <div className={Style.header}>
-                        <header>
-                            <h1>Cart</h1>
-                        </header>
-                    </div>
-                    <div className={Style.main}>
-                        <main>
-        
-                        </main>
-                    </div>
-                    <div className={Style.footer}>
-                        <footer>
-                        <Button
-                            isDisabled={false}
-                            size="medium"
-                            variant="primary"
-                            isLoading={false}
-                            text="Checkout"
-                            className="additionalClass"
-                            />
-                            <Button
-                            isDisabled={false}
-                            size="medium"
-                            variant="primary"
-                            isLoading={false}
-                            text="Continue Shopping"
-                            className="additionalClass"
-                            />
-                        </footer>
-                    </div>
-                </div>
-        ) : (
-            <></>
-        )}
+      {isAsideVisible ? (
+        <div className={Style.container}>
+          <div className={Style.header}>
+            <header>
+              <h1>Cart</h1>
+            </header>
+          </div>
+          <div className={Style.main}>
+            <main>
+                <h1>This is my content</h1>
+            </main>
+          </div>
+          <div className={Style.footer}>
+            <footer>
+              <Button
+                onClick={hideAside}
+                isDisabled={false}
+                size="small"
+                variant="primary"
+                isLoading={false}
+                text="Checkout"
+                className="additionalClass"
+              />
+              <br />
+                <Button
+                onClick={hideAside}
+                isDisabled={false}
+                size="small"
+                variant="primary"
+                isLoading={false}
+                text="Continue Shopping"
+                className="additionalClass"
+              />
+              {/* Add more buttons or content as needed */}
+            </footer>
+          </div>
+        </div>
+      ) : (
+        <></>
+      )}
     </React.Fragment>
-  )
-}
+  );
+};
