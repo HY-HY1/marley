414ab891736bdf72871f22335543cfe5dcb64822
Harvey Yemm <hjyemm@icloud.com>
2023-12-29 10:04:53 +0000

    Refactored map function

--- client/src/page/Cart.jsx
+++ client/src/page/Cart.jsx
@@ -1,20 +1,49 @@
-// Cart.js
 import React from 'react';
 import { useShop } from '../context/ShopContext';
 import { CartItem } from '../component/CartItem';
 import { StripeCheckout } from '../utils/StripeCheckout';
+import Style from '../style/cart.module.css';
+import { Button } from '../component/Button';
 
 export const Cart = () => {
   const { cartItems, removeFromCart, clearCart, calculateTotalPrice } = useShop();
 
   return (
     <div>
-      <h2>Shopping Cart</h2>
       {cartItems.map((item) => (
-        <CartItem key={item.id} id={item.id} onRemove={() => removeFromCart(item.id)} />
+        <CartItem key={item.id} id={item.id} quantity={item.quantity} onRemove={() => removeFromCart(item.id)} />
       ))}
-      <p>Total Price: {calculateTotalPrice()}</p>
-      <StripeCheckout amount={calculateTotalPrice()} />
+      {cartItems.length > 0 ? (
+        <div className={Style.checkout}>
+          <ul>
+            <li>
+              <div className={Style.total}>
+                <p>Total: £{calculateTotalPrice()}</p>
+              </div>
+            </li>
+            <li>
+              <div className={Style.checkoutBtn}>
+                <StripeCheckout amount={calculateTotalPrice() } />
+              </div>
+            </li>
+          </ul>
+        </div>
+      ) : (
+        <div className={Style.empty}>
+          <h1>Your Cart is empty</h1>
+          <div className={Style.shop}>
+            <Button
+              link={'/'}
+              isDisabled={false}
+              size="medium"
+              variant="primary"
+              isLoading={false}
+              text="Shop Now"
+              className="additionalClass"
+            />
+          </div>
+        </div>
+      )}
     </div>
   );
 };
