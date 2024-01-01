a9ad304299b0949a6de52cb8e0acc15ffa97c6dc
Harvey Yemm <hjyemm@icloud.com>
2024-01-01 23:42:34 +0000

    Updated Form Props

--- client/src/page/Account.jsx
+++ client/src/page/Account.jsx
@@ -1,10 +1,46 @@
-import React from 'react'
-import { Auth } from '../utils/Auth'
+import React, { useState } from 'react';
+import { Form } from '../component/Form';
+import { FormProvider } from '../component/Form';
+import Auth from '../utils/Auth'; // Import Auth
 
 export const Account = () => {
+  const [choice, setChoice] = useState(true);
+  const login = [
+    { title: 'email' },
+    { title: 'password' },
+    // Add more input configurations as needed
+  ];
+
+  const register = [
+    { title: 'email' },
+    { title: 'password' },
+    { title: 'name' },
+    // Add more input configurations as needed
+  ];
+
+  const inputs = choice ? login : register;
+
+  let postUrl;
+
+  if (choice) {
+    postUrl = '/account/login';
+  } else {
+    postUrl = '/account/register';
+  }
+
+  console.log(postUrl);
+
   return (
     <React.Fragment>
-      <Auth/>
+      <FormProvider>
+        {/* Pass 'postUrl' as a prop to the Form component */}
+        <Form inputs={inputs} postURL={postUrl} />
+        {choice ? (
+          <p onClick={() => setChoice(!choice)}>Register</p>
+        ) : (
+          <p onClick={() => setChoice(!choice)}>Login</p>
+        )}
+      </FormProvider>
     </React.Fragment>
-  )
-}
+  );
+};
