bceced05f160e029f000dbd8ec84fd4e277025b2
Harvey Yemm <hjyemm@icloud.com>
2024-01-01 23:53:54 +0000

    Refactored Auth Util for readability

--- client/src/utils/Auth.js
+++ client/src/utils/Auth.js
@@ -1,58 +1,40 @@
-import React, { useEffect, useState } from 'react';
+// auth.js
 import axios from 'axios';
 
-export const Auth = () => {
-    const [loading, setLoading] = useState(true);
+export const Auth = async () => {
+  const token = localStorage.getItem('token');
 
-    const token = localStorage.getItem('token');
+  if (!token) {
+    console.log('No Token Provided');
+    window.location.href = '/account';
+    return;
+  }
 
-    useEffect(() => {
-        const AuthUser = async () => {
-            try {
-                if (!token) {
-                    setLoading(false);
-                    console.log('No Token Provided')
-                    window.location.href = '/account/login';
-                    return;
-                }
-
-                const response = await axios.post(
-                    'http://localhost:3001/account/dashboard',
-                    {},
-                    {
-                        headers: {
-                            Authorization: token,
-                        },
-                    }
-                );
-
-                if (response.status !==200) {
-                    setLoading(false);
-                    window.location.href = '/account/login';
-                    console.log('Server Had a red response')
-                    return;
-                }
-
-                setLoading(false);
-                window.location.href = '/dashboard';
-            } catch (error) {
-                window.location.href = '/account/login';
-                console.log('There was an error', error)
-                setLoading(false);
-            }
-        };
-
-        AuthUser();
-    }, []); 
-
-    return (
-        <div>
-            {loading ? (
-                // Show a loading indicator or message while waiting for the authentication check
-                <p>Loading...</p>
-            ) : (
-                <p></p>
-            )}
-        </div>
+  try {
+    const response = await axios.post(
+      'http://localhost:3001/account/dashboard',
+      {},
+      {
+        headers: {
+          Authorization: token,
+        },
+      }
     );
+
+    if (response.status !== 200) {
+      console.log('Server Had a red response');
+      window.location.href = '/account';
+      return;
+    }
+
+    window.location.href = '/dashboard';
+  } catch (error) {
+    console.log('There was an error', error);
+    window.location.href = '/account';
+  }
+  return (
+    <>
+      <p>Loading</p>
+    </>
+  )
 };
