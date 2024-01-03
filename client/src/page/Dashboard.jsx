cf18db8126193be80f4fb5b2787115e130eb6ca8
Harvey Yemm <hjyemm@icloud.com>
2024-01-03 11:50:11 +0000

    Wrapped dashboard page in User Provider

--- client/src/page/Dashboard.jsx
+++ client/src/page/Dashboard.jsx
@@ -1,7 +1,20 @@
 import React from 'react'
+import { UserProvider, useUser } from '../context/UserContext'
 
 export const Dashboard = () => {
   return (
-    <div>Dashboard</div>
+    <UserProvider>
+      <DashboardPage/>
+    </UserProvider>
+  )
+}
+
+export const DashboardPage = () => {
+  const { user } = useUser()
+  return (
+    <>
+    <h3>{user.name}</h3>
+    <p>{user.email}</p>
+    </>
   )
 }
