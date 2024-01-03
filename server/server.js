f98e8c2a95996e943672e3a5bfaa2a614bf1f219
Harvey Yemm <hjyemm@icloud.com>
2024-01-03 20:53:42 +0000

    Updated import routes

--- server/server.js
+++ server/server.js
@@ -41,9 +41,9 @@ const URI = process.env.URI;
 const product = require('./route/product')
 const payment = require('./route/payment')
 const account = require('./route/account')
-const order = require('./route/orders')
+const stripe = require('./route/stripe')
 
 app.use('/product', product)
 app.use('/payment', payment)
 app.use('/account', account)
-app.use('/order', order)
+app.use('/stripe', stripe)
