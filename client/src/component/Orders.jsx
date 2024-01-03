8cf07c92cc4de7aeb1cd97dd5eb28f08d6ce05ee
Harvey Yemm <hjyemm@icloud.com>
2024-01-03 11:57:32 +0000

    Displays users past orders

---
+++ client/src/component/Orders.jsx
@@ -0,0 +1,9 @@
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+import { useUser } from '../context/UserContext'
+
+export const Orders = () => {
+  return (
+    <div>Orders</div>
+  )
+}
