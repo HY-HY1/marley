e5737e3d6aa5495adcf2b841d20aa803aff5dfd7
Harvey Yemm <hjyemm@icloud.com>
2024-01-04 23:08:26 +0000

    Added order component to page

--- client/src/page/Dashboard.jsx
+++ client/src/page/Dashboard.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import { UserProvider, useUser } from '../context/UserContext'
+import { Button } from '../component/Button'
+import { Orders } from '../component/Orders'
 
 export const Dashboard = () => {
   return (
@@ -10,11 +12,19 @@ export const Dashboard = () => {
 }
 
 export const DashboardPage = () => {
-  const { user } = useUser()
+  const { user, logout } = useUser()
   return (
     <>
     <h3>{user.name}</h3>
     <p>{user.email}</p>
+      <Button
+      text='Log Out'
+      size='m'
+      variant='danger'
+      onClick={logout}
+      link='/'
+      />
+      <Orders/>
     </>
   )
 }
